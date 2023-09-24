25f21a341573587497b88880065604b98ee7e84b
liwei <3144028559@qq.com>
2023-09-24 13:54:11 +0800

    day07

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,12 +1,23 @@
-import request from "@/utils/http"
+// 封装所有和用户相关的接口函数
+import request from '@/utils/http'
 
-export const loginAPI = ({account,password}) => {
+export const loginAPI = ({ account, password }) => {
     return request({
         url: '/login',
         method: 'POST',
-        data:{
+        data: {
             account,
             password
+        }
+    })
+}
+
+
+export const getLikeListAPI = ({ limit = 4 }) => {
+    return request({
+        url: '/goods/relevant',
+        params: {
+            limit
         }
     })
 }
